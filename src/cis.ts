9597deb9981e23d690ddfd95a1ba64ddbd3fa680
xerysherry <xerysherry@aliyun.com>
2018-01-16 00:03:04 +0800

    remove unused code

--- src/cis.ts
+++ src/cis.ts
@@ -1,6 +1,20 @@
+/* Copyright xerysherry 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import Colors = require("colors.ts");
 import getPixels = require("get-pixels");
-import { colors } from "colors.ts";
 
 function GetPixels(pixels, pw, ph, x, y, w, h) {
     let r = 0;
